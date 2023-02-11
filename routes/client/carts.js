a09c5c73306e8eef6a3b71719a6cfd7e3ff60d5a
ManhNguyen241196 <nguyenduymanh241196@gmail.com>
2023-02-11 18:55:08 +0700

    show page gio hang và ket huc du an

--- routes/client/carts.js
+++ routes/client/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartRepo = require('../../respositories/carts')
+const productsRepo = require('../../respositories/products')
+const cartShowTemplate = require('../../views/admin/carts/show')
 
 const router = express.Router();
 
@@ -8,9 +10,9 @@ const router = express.Router();
 //     res.send('đã reset cart id')
 // })
 
-router.get('/getCartId', (req,res)=>{
-    res.send(`cardId cua gio hang la: ${req.session.cartId}`)
-})
+// router.get('/getCartId', (req,res)=>{
+//     res.send(`cardId cua gio hang la: ${req.session.cartId}`)
+// })
 
 router.post('/cart/products', async (req,res)=>{
     let cart;  
@@ -20,7 +22,7 @@ router.post('/cart/products', async (req,res)=>{
     
     if(!req.session.cartId){  //cartId chi được tạo khi người dùng thực sự nhấp vào button cart.
       cart = await cartRepo.create({items : []})
-      req.session.cartId = cart.id
+      req.session.cartId = cart.id //nếu không có cookies thì sẽ tạo bằng cách gán cho nó id của cart dk tạo trong respoProduct.creat()
     }else{
        cart = await cartRepo.getOne(req.session.cartId); //tìm ra phần tử trong array cart
         //thỏa mãn điều kiện nào đó. để nếu cart đã tồn tại, ng dùng đã nhấn và tạo 1 kết nối cartId bằng cookies với session của server 
@@ -44,6 +46,39 @@ router.post('/cart/products', async (req,res)=>{
   console.log(req.session)
   await cartRepo.update(cart.id, {items: cart.items} )
   res.redirect('/')
+})
+
+router.get('/cart', async (req,res)=>{
+   if(!req.session.cartId){
+      return res.redirect('/');
+   }
+
+   //nếu không exist req.session.cartId thì chứng tỏ chưa tồn tại giỏ hàng nên cũng k có gì để show cả
+   const cart = await cartRepo.getOne(req.session.cartId)
+
+   for (let item of cart.items){  //items tồn tại dưới dạng 1 array có các phần tử là các object chứa đặc tính của sản phẩm.
+      //luc nay item cos dajn g object {id: id cua product, quanity: số lượng sản phẩm}
+      const product = await productsRepo.getOne(item.id);
+      
+      item.product = product;  // gán thêm vào object trên 1 thuoc tính product có chứa object trọn vẹn của 
+      //product trong khu vực lưu trữ gốc. 
+   }
+   res.send(cartShowTemplate({  // luôn đưa biến về trạng thái 1 object để sau nếu có thêm sẽ dễ quản lí . 
+      items: cart.items,
+   }))
+})
+
+router.post('/cart/products/delete', async (req,res)=>{
+   const {itemId} = req.body
+   const cart = await cartRepo.getOne(req.session.cartId)
+
+   const items = cart.items.filter(function (item) {
+     return  item.id !== itemId 
+   })
+
+   await cartRepo.update(req.session.cartId,{items})
+
+   res.redirect('/cart')
 })
 
 module.exports = router;
